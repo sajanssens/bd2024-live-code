2cbf61b0af974414eaa0b795b1f2afbcebf5a533
Bram Janssens <s.a.janssens@gmail.com>
2024-09-11 16:06:54 +0200

    Small update.

--- src/main/java/com/infosupport/h3/NumericValues.java
+++ src/main/java/com/infosupport/h3/NumericValues.java
@@ -2,8 +2,7 @@ package com.infosupport.h3;
 
 public class NumericValues {
 
-
-    public void eenFunctie(String[] args) {
+    public void eenFunctie() {
         byte number = 127;
         byte number2 = 1;
 
