f87716b08366ee6902efd25d8fd9eb3e19cbc3e3
Bram Janssens <s.a.janssens@gmail.com>
2024-11-18 14:37:06 +0100

    casino without bugs.

--- blok3/ts/lab1/casino.js
+++ blok3/ts/lab1/casino.js
@@ -1,9 +1,10 @@
+// @ts-check
 class Person {
     /**
      * @param {string} name
      */
     constructor(name) {
-        this._name = name;
+        this.name = name;
     }
 }
 
@@ -14,23 +15,23 @@ class Player extends Person {
      * @param {number} chips
      */
     constructor(name, chips) {
+        super(name);
         this.chips = chips;
     }
 
     toString() {
-        return `${this.name} has ${chips} number of chips left`;
+        return `${this.name} has ${this.chips} number of chips left`;
     }
 }
 
-var playerOne = new Player('Han', 46);
-var playerTwo = new Player('Leia', 68);
 
-var highestNumberOfChips = Math.max([playerOne.chips, playerTwo.chips]);
-console.log(highestNumberOfChips + ' is the highest number of chips');
 
 class RouletteBoard {
     constructor() {
-        this.betRecords = [];
+        /**
+         * @type {any[]}
+         */
+        this.bets = [];
     }
 
     /**
@@ -38,39 +39,44 @@ class RouletteBoard {
      * @param {Player} player
      * @param {number} bet
      */
-    placeBet(player, bet) {
-        var record = this.records.find((r) => r.player === player && r.bet === bet);
+    placeBetOneChip(player, bet) {
+        let record = this.bets.find((r) => r.player === player && r.bet === bet);
         if (!record) {
-            record = { player: player, bet: bet, numberOfChips: 0 };
-            this.betRecords.add(record);
+            record = {player: player, bet: bet, numberOfChips: 0};
+            this.bets.push(record);
         }
         record.numberOfChips++;
     }
 
     play() {
-        var winner = Math.floor(Math.random() * 36);
-        console.log('winning number: ' + winner);
-        for (var record in this.betRecords) {
-            if (this.betRecords[record].bet === winner) {
-                var loot = this.betRecords[record].numberOfChips * 10;
-                this.betRecords[record].player.chips += loot;
-                console.log(
-                    this.betRecords[record].player.toString() + ' wins ' + loot,
-                );
-            }
-        }
-        this.betRecords = [];
+        const winningNumber = Math.floor(Math.random() * 36)
+
+        this.bets
+            .filter(b => b.bet === winningNumber)
+            .forEach(b => {
+                const loot = b.numberOfChips * 10
+                b.player.chips += loot
+                console.log(`${b.player} wins ${loot}`)
+            })
+
+        this.bets = []
     }
 }
 
-var roulette = new RouletteBoard();
-roulette.placeBet(playerOne, 20);
-roulette.placeBet(playerOne, 20);
-roulette.placeBet(playerTwo, 1);
-roulette.placeBet(playerTwo, 2);
-roulette.placeBet(playerTwo, 6);
-roulette.placeBet(playerTwo, 31);
-roulette.placeBet(playerTwo, 5);
-roulette.placeBet(playerTwo, 4);
+const playerOne = new Player('Han', 46);
+const playerTwo = new Player('Leia', 68);
+const highestNumberOfChips = Math.max(playerOne.chips, playerTwo.chips);
+console.log(highestNumberOfChips + ' is the highest number of chips');
+
+const roulette = new RouletteBoard();
+
+roulette.placeBetOneChip(playerOne, 20);
+roulette.placeBetOneChip(playerOne, 20);
+roulette.placeBetOneChip(playerTwo, 1);
+roulette.placeBetOneChip(playerTwo, 20);
+roulette.placeBetOneChip(playerTwo, 6);
+roulette.placeBetOneChip(playerTwo, 31);
+roulette.placeBetOneChip(playerTwo, 5);
+roulette.placeBetOneChip(playerTwo, 4);
 
 roulette.play();
