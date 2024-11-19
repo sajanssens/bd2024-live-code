debb9f0d3a82e28ddd5239ca89cf1636d0703408
Bram Janssens <s.a.janssens@gmail.com>
2024-11-19 09:13:46 +0100

    Removed js.

--- blok3/ts/Day1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Compass = void 0;
-var Compass;
-(function (Compass) {
-    Compass[Compass["North"] = 0] = "North";
-})(Compass || (exports.Compass = Compass = {}));
