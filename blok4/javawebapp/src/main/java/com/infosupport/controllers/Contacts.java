fd830d81a85a0fd925c5ad3efeb79bf9f4d5e778
Bram Janssens <s.a.janssens@gmail.com>
2024-11-26 13:55:57 +0100

    Get contacts.

--- blok4/javawebapp/src/main/java/com/infosupport/controllers/Contacts.java
+++ blok4/javawebapp/src/main/java/com/infosupport/controllers/Contacts.java
@@ -15,8 +15,33 @@ public class Contacts extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.println("""
-                {'name': "Bram"}
-                """);
+                    [
+                        {
+                          "id": 4,
+                          "firstName": "Sam From Tomcat",
+                          "surname": "Smith",
+                          "email": "sam.smith@music.com"
+                        },
+                        {
+                          "id": 8,
+                          "firstName": "Frank",
+                          "surname": "Muscles",
+                          "email": "frank@muscles.com"
+                        },
+                        {
+                          "id": 16,
+                          "firstName": "Ramon",
+                          "surname": "Arnhem",
+                          "email": "ram@n.nl"
+                        },
+                        {
+                          "id": 17,
+                          "firstName": "Bram",
+                          "surname": "Janssens",
+                          "email": "s.a.janssens@gmail.com"
+                        }
+                    ]
+                """.trim().replaceAll("\n", "").replaceAll("\r", ""));
         // super.doGet(req, resp);
     }
 }
