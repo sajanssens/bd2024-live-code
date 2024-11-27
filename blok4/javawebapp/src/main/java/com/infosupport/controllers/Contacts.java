5246a0d1a62431c5b7a0bca0da848aad4669278b
Bram Janssens <s.a.janssens@gmail.com>
2024-11-27 15:47:06 +0100

    Added my first rest app.

--- blok4/javawebapp/src/main/java/com/infosupport/controllers/Contacts.java
+++ blok4/javawebapp/src/main/java/com/infosupport/controllers/Contacts.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/contacts") // or in web.xml
+// @WebServlet("/contacts") // or in web.xml
 public class Contacts extends HttpServlet {
 
     @Override
