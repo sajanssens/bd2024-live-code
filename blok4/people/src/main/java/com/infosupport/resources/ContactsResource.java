e030855b6c0d171c4d063e5e1294d4e17b1e6529
Bram Janssens <s.a.janssens@gmail.com>
2024-11-28 11:13:45 +0100

    QueryParam added.

--- blok4/people/src/main/java/com/infosupport/resources/ContactsResource.java
+++ blok4/people/src/main/java/com/infosupport/resources/ContactsResource.java
@@ -5,6 +5,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -15,17 +17,18 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 public class ContactsResource {
 
     @GET
-    public List<Contact> all() {
+    public List<Contact> all(@QueryParam("q") String q) {
         return List.of(
-                new Contact(1, "Bram1", "Janssens", "45@42.com"),
+                new Contact(1, "Bram1", "Janssens" + q, "45@42.com"),
                 new Contact(2, "Bram2", "Janssens", "45@42.com"),
-                new Contact(22, "Bram2", "Janssens", "45@42.com"),
-                new Contact(3, "Bram3", "Janssens", "45@42.com"));
+                new Contact(3, "Bram3", "Janssens", "45@42.com"),
+                new Contact(4, "Bram4", "Janssens", "45@42.com"));
     }
 
-    @GET
-    @Path("{id}")
-    public Contact two(@PathParam("id") int id) {
-        return new Contact(id, "Bram", "Janssens", "45@42.com");
+    @GET @Path("{id}")
+    public Response one(@PathParam("id") int id) {
+        return Response.ok()
+                .entity(new Contact(id, "Bram", "Janssens", "45@42.com"))
+                .build();
     }
 }
