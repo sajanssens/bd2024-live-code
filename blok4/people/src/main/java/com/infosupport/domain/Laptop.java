3ee11761a9390d235304df242d8678034126be4a
Bram Janssens <s.a.janssens@gmail.com>
2024-12-18 20:42:28 +0100

    Added docker compose for people rest service with mysql.

--- blok4/people/src/main/java/com/infosupport/domain/Laptop.java
+++ blok4/people/src/main/java/com/infosupport/domain/Laptop.java
@@ -1,4 +1,17 @@
 package com.infosupport.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor
+@Builder @AllArgsConstructor
 public class Laptop {
+    @Id @GeneratedValue
+    private int id;
 }
